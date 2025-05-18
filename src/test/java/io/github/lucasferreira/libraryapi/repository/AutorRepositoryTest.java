27c4fca48f390f583a4c640b6b5e4e74585ad3e6
Lucas <lucas.iferreir@gmail.com>
2025-05-17 21:05:13 -0300

    Aplicando Testes com JUnit, deletando por objeto, contagem de objetos, e listar objetos

--- src/test/java/io/github/lucasferreira/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/lucasferreira/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,8 +28,18 @@ public class AutorRepositoryTest {
 
     @Test
     public void deletarTest() {
-        Autor autor = new Autor();
-        repository.deleteById(autor.getId());
+        var id = UUID.fromString("00708a9e-8f83-4bd5-a8bf-3fff9873d358");
+        repository.deleteById(id);
+
+        System.out.println("Usuario deletado");
+    }
+
+    @Test
+    public void deletarObjetoTest() {
+        var id = UUID.fromString("47e112a2-5842-4b6d-8e10-6e0053ac7c67");
+        var autor = repository.findById(id).get();
+
+        repository.delete(autor);
 
         System.out.println("Usuario deletado");
     }
@@ -45,8 +56,19 @@ public class AutorRepositoryTest {
             autorEncontrado.setDataNascimento(LocalDate.of(1960, 1, 30));
 
             repository.save(autorEncontrado);
-
-
         }
     }
+
+    @Test
+    public void listarTest(){
+        List<Autor> autorList = repository.findAll();
+
+        autorList.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest(){
+        System.out.println("Contagem: " + repository.count());
+    }
+
 }
