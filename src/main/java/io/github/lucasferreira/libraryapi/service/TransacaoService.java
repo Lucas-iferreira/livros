5edf6eb8aaac30814a1d36f21e3c2b079b03430f
Lucas <lucas.iferreir@gmail.com>
2025-05-19 22:43:04 -0300

    Entendendo como funcionam as transações do Spring, como são os estados das entidades e executando um test

---
+++ src/main/java/io/github/lucasferreira/libraryapi/service/TransacaoService.java
@@ -0,0 +1,51 @@
+package io.github.lucasferreira.libraryapi.service;
+
+import io.github.lucasferreira.libraryapi.model.Autor;
+import io.github.lucasferreira.libraryapi.model.GeneroLivro;
+import io.github.lucasferreira.libraryapi.model.Livro;
+import io.github.lucasferreira.libraryapi.repository.AutorRepository;
+import io.github.lucasferreira.libraryapi.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Service
+public class TransacaoService {
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @Transactional
+    public void executar() {
+        //salva o autor
+        Autor autor = new Autor();
+        autor.setNome("Teste Francisco");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1971, 5, 23));
+
+        autorRepository.save(autor);
+
+
+        //salva o livro
+        Livro livro = new Livro();
+        livro.setIsbn("9888845-1234");
+        livro.setPreco(BigDecimal.valueOf(300));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Teste Livro da Francisca");
+        livro.setDataPublicacao(LocalDate.of(1992, 4, 29));
+
+        livro.setAutor(autor);
+
+        livroRepository.save(livro);
+
+        if(autor.getNome().equals("Teste Francisco")){
+            throw new RuntimeException("Rollback");
+        };
+
+    }
+}
---
+++ src/test/java/io/github/lucasferreira/libraryapi/repository/TransacoesTest.java
@@ -0,0 +1,24 @@
+package io.github.lucasferreira.libraryapi.repository;
+
+import io.github.lucasferreira.libraryapi.service.TransacaoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class TransacoesTest {
+
+    @Autowired
+    TransacaoService transacaoService;
+
+    /**
+     * Commit -> confirmar as alterações
+     * Rollvack -> desfazer as alterações
+     */
+    @Test
+    void transacaoSimples(){
+        transacaoService.executar();
+
+    }
+}
