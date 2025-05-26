a4ad019778edb51080b3fec514a63778b8baa3c7
Lucas <lucas.iferreir@gmail.com>
2025-05-26 09:33:06 -0300

    Testando o endpoint Post

--- src/main/java/io/github/lucasferreira/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/lucasferreira/libraryapi/controller/AutorController.java
@@ -1,10 +1,21 @@
 package io.github.lucasferreira.libraryapi.controller;
 
 
+import io.github.lucasferreira.libraryapi.controller.dto.AutorDTO;
+import io.github.lucasferreira.libraryapi.model.Autor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/autores")
 public class AutorController {
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody AutorDTO autor) {
+        return new ResponseEntity("Autor salvo com sucesso! " + autor, HttpStatus.CREATED);
+    }
 }
