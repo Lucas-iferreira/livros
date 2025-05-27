c3c7c31ea5d12f01da4a693e1cf94e1f2747583a
Lucas <lucas.iferreir@gmail.com>
2025-05-27 09:40:43 -0300

    Aplicando as validations com exceptions handler

--- src/main/java/io/github/lucasferreira/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/lucasferreira/libraryapi/controller/dto/AutorDTO.java
@@ -3,6 +3,8 @@ package io.github.lucasferreira.libraryapi.controller.dto;
 import io.github.lucasferreira.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,13 +12,16 @@ import java.util.UUID;
 public record AutorDTO(
         UUID id,
         @NotBlank(message = "Campo obrigatorio")
+        @Size(min = 2, max = 100, message = "campo fora do tamanho padrão")
         String nome,
         @NotNull(message = "Campo obrigatorio")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataNascimento,
         @NotBlank(message = "Campo obrigatorio")
+        @Size(min = 2, max = 50, message = "campo fora do tamanho padrão")
         String nacionalidade) {
 
-    public Autor mapearParaAutor(){
+    public Autor mapearParaAutor() {
         Autor autor = new Autor();
         autor.setNome(this.nome);
         autor.setDataNascimento(this.dataNascimento());
